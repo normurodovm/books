0dfb9c9a0c9a22fc3c0e697cf36a8f2540dba0f7
normurodovm <nmaqsud302@gmail.com>
2024-09-06 15:47:46 +0500

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className=" bg-red-400 w-[500px] mx-auto mt-[150px] p-[50px] rounded-3xl">
+      <input className="w-full p-5 border mb-4 rounded-xl" placeholder="Enter your email" type="email" />
+      <input className="w-full p-5 border mb-4 rounded-xl" placeholder="Enter your password" type="password" />
+      <Link href={"/product"}>
+        <button className="w-full p-5 border bg-green-500 hover:bg-green-600 rounded-xl text-white" type="submit">Kirish</button>
+      </Link>
     </div>
   );
 }
